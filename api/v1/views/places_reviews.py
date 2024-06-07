489d794acedb22d363b0768347dbb17785681cf7
Matsadura <zidanditoo1@gmail.com>
2024-06-08 00:05:48 +0100

    [UPDATE] Revert changes

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -18,9 +18,8 @@ def review_of_a_place(place_id):
         return jsonify([review.to_dict() for review in place.reviews])
 
     if request.method == 'POST':
-        try:
-            data = request.get_json()
-        except Exception as e:
+        data = request.get_json(silent=True)
+        if not data:
             abort(400, 'Not a JSON')
         if 'user_id' not in data.keys():
             abort(400, 'Missing user_id')
@@ -55,9 +54,8 @@ def review_id(review_id):
     if request.method == 'PUT':
         if not obj:
             abort(404)
-        try:
-            data = request.get_json()
-        except Exception as e:
+        data = request.get_json(silent=True)
+        if not data:
             abort(400, 'Not a JSON')
         skippable = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
         for k, v in data.items():
